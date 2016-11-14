b6238bd2d7c8027d2122cb95fcfc3d8448a65587
Uwe Klawitter <uweklawitter@cowabuja.de>
2016-11-14 12:33:10 +0100

    addedc copytree.js

---
+++ www/copytree.js
@@ -0,0 +1,16 @@
+var copytreeName = 'CopyTree';
+
+function CopyTree() {
+}
+
+CopyTree.prototype = {
+    copyToInternal: function() {
+        cordova.exec(success, fail, copytreeName, "copyToInternal");
+    }
+
+    copyToExternal: function() {
+        cordova.exec(success, fail, copytreeName, "copyToExternal");
+    }
+};
+
+module.exports = new CopyTree();
