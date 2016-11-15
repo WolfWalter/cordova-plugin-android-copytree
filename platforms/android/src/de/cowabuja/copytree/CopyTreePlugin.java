e27165b9fb02809e6136c567b13de886846546dd
Uwe Klawitter <uweklawitter@cowabuja.de>
2016-11-15 14:43:59 +0100

    integration fix

--- platforms/android/src/de/cowabuja/copytree/CopyTreePlugin.java
+++ platforms/android/src/de/cowabuja/copytree/CopyTreePlugin.java
@@ -6,7 +6,9 @@ import android.support.v4.provider.DocumentFile;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,52 +22,76 @@ public class CopyTreePlugin extends CordovaPlugin {
     private static final String ACTION_COPY_TO_INTERNAL = "copyToInternal";
     private static final String ACTION_COPY_TO_EXTERNAL = "copyToExternal";
     private static final int ACTION_COPY_TO_EXTERNAL_CODE = 55;
+    private DocumentFile cacheDir;
     private DocumentFile externalFile;
     private DocumentFile internalFile;
     private CallbackContext callback;
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Log.i(TAG, "execute");
-        boolean result = false;
-        Activity activity = cordova.getActivity();
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        cacheDir = DocumentFile.fromFile(cordova.getActivity().getCacheDir());
+    }
 
-        DocumentFile cacheDir = DocumentFile.fromFile(activity.getCacheDir());
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        boolean result = false;
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+        callback = callbackContext;
 
         if (action.equals(ACTION_COPY_TO_INTERNAL)) {
-            internalFile = cacheDir.createDirectory(TREE_EXPORT_DIR);
-            for (DocumentFile child : internalFile.listFiles()) {
-                child.delete();
-            }
-            Log.i(TAG, "internal directory: " + internalFile.getUri());
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    copyToInternal();
+                }
+            });
 
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            cordova.startActivityForResult(this, intent, ACTION_COPY_TO_EXTERNAL_CODE);
-            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
-            callback = callbackContext;
             result = true;
         } else if (action.equals(ACTION_COPY_TO_EXTERNAL)) {
-            if (externalFile == null) {
-                Log.e(TAG, "external file not defined");
-            }
-
-            try {
-                for (DocumentFile child : externalFile.listFiles()) {
-                    child.delete();
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    copyToExternal();
                 }
+            });
 
-                CopyService.copy(activity.getContentResolver(), internalFile, externalFile, true);
-                callbackContext.success();
-                result = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                result = false;
-            }
+            result = true;
         }
 
         return result;
+    }
+
+    private void copyToExternal() {
+        if (externalFile == null) {
+            Log.e(TAG, "external file not defined");
+        }
+
+        try {
+            for (DocumentFile child : externalFile.listFiles()) {
+                child.delete();
+            }
+
+            CopyService.copy(cordova.getActivity().getContentResolver(), internalFile, externalFile, true);
+            callback.success();
+        } catch (IOException e) {
+            e.printStackTrace();
+            callback.error(e.getMessage());
+        }
+    }
+
+    private void copyToInternal() {
+        internalFile = cacheDir.createDirectory(TREE_EXPORT_DIR);
+        for (DocumentFile child : internalFile.listFiles()) {
+            child.delete();
+        }
+        Log.i(TAG, "internal directory: " + internalFile.getUri());
+
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        cordova.startActivityForResult(this, intent, ACTION_COPY_TO_EXTERNAL_CODE);
     }
 
     @Override
